3783d65fac0ed0d8956a44dd821587aa2738549a
unknown <mohaiminislam94@gmail.com>
2019-07-10 01:29:06 +0600

    complete nav for big screen size

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../static/shopping-bag.svg';
 
 const Nav = () => {
   return ( 
@@ -9,8 +10,18 @@ const Nav = () => {
       <div className="col-2">
       </div>
       <div className="col-3"></div>
-      <div className="col-4">col-4</div>
-      <div className="col-5">col-5</div>
+      <div className="col-4">
+        <div className="nav-menu">
+          <div className="nav-menu--item"> <a className="active" href="/home">Home</a> </div>
+          <div className="nav-menu--item"> <a href="/home">Strategy</a></div>
+          <div className="nav-menu--item"> <a href="/home">About</a></div>
+          <div className="nav-menu--item"> <a href="/home">Clients</a></div>
+          <div className="nav-menu--item"> <a href="/home">Sign up</a></div>
+        </div>
+      </div>
+      <div className="col-5">
+        <Logo className="shooping-cart" />
+      </div>
     </nav>
   );
 }
