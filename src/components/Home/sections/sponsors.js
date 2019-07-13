60c61a794f556fe0bd4f961e8a2ef76338e40514
unknown <mohaiminislam94@gmail.com>
2019-07-13 23:17:40 +0600

    finish sponsor section animation

--- src/components/Home/sections/sponsors.js
+++ src/components/Home/sections/sponsors.js
@@ -3,6 +3,7 @@ import sponsorLogoOne from '../../../static/sponsor-1.png';
 import sponsorLogoTwo from '../../../static/sponsor-2.png';
 import sponsorLogoThree from '../../../static/sponsor-3.png';
 import sponsorLogoFour from '../../../static/sponsor-4.png';
+import ScrollAnimation from 'react-animate-on-scroll';
 
 const Sponsors = () => {
   return (
@@ -12,12 +13,12 @@ const Sponsors = () => {
       <div className="col-3"></div>
       <div className="col-4"></div>
       <div className="col-5"></div>
-      <div className="sponsor-gallery">
+      <ScrollAnimation className="sponsor-gallery"   animateIn="fadeInUp" duration={.5} animateOnce>
         <img src={sponsorLogoOne} alt="sponsor-gallery--logo" className="sponsor-gallery--logo"/>
         <img src={sponsorLogoTwo} alt="sponsor-gallery--log" className="sponsor-gallery--logo"/>
         <img src={sponsorLogoThree} alt="sponsor-gallery--log" className="sponsor-gallery--logo"/>
         <img src={sponsorLogoFour} alt="sponsor-gallery--logo" className="sponsor-gallery--logo"/>
-      </div>
+      </ScrollAnimation>
     </section>
     
    );
