790aff587e3fcbb60e72eb0701f157f5204165cb
unknown <mohaiminislam94@gmail.com>
2019-07-15 23:21:01 +0600

    Updates

---
+++ precache-manifest.1f01fe7b028abf816e52ed00494c8174.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991c91268b5ec7a579e8505a0e064521",
+    "url": "/frontend-maestro/index.html"
+  },
+  {
+    "revision": "5011ce7c264c7d9f774b",
+    "url": "/frontend-maestro/static/css/2.f2ad0615.chunk.css"
+  },
+  {
+    "revision": "2676e0b2be5749bb8df3",
+    "url": "/frontend-maestro/static/css/main.4f3b41ef.chunk.css"
+  },
+  {
+    "revision": "5011ce7c264c7d9f774b",
+    "url": "/frontend-maestro/static/js/2.5f11e349.chunk.js"
+  },
+  {
+    "revision": "2676e0b2be5749bb8df3",
+    "url": "/frontend-maestro/static/js/main.f2ee0236.chunk.js"
+  },
+  {
+    "revision": "8681d4bac4804f4bb3b6",
+    "url": "/frontend-maestro/static/js/runtime~main.f0a07af2.js"
+  },
+  {
+    "revision": "d0e2a772a5288588d62e6418f2b6e960",
+    "url": "/frontend-maestro/static/media/close.d0e2a772.svg"
+  },
+  {
+    "revision": "02293af145340b0a735f14a460364e6b",
+    "url": "/frontend-maestro/static/media/header-bg.02293af1.png"
+  },
+  {
+    "revision": "a8c6e39c3572e84c0d1be93b4e9858d4",
+    "url": "/frontend-maestro/static/media/menu.a8c6e39c.svg"
+  },
+  {
+    "revision": "81e1991430227974bca5e7f6ebe41d72",
+    "url": "/frontend-maestro/static/media/new-art.81e19914.png"
+  },
+  {
+    "revision": "e3bfac3a16f4f71c172a7fcb578d785c",
+    "url": "/frontend-maestro/static/media/next-bg.e3bfac3a.jpg"
+  },
+  {
+    "revision": "b2abe575c62557f273f7f288c58f7218",
+    "url": "/frontend-maestro/static/media/next-iphone.b2abe575.png"
+  },
+  {
+    "revision": "24e14199c727d9e2232d4428879cbbbd",
+    "url": "/frontend-maestro/static/media/next-main-bg.24e14199.png"
+  },
+  {
+    "revision": "b7e017c0bec9837556e420bf334a5475",
+    "url": "/frontend-maestro/static/media/our-programs-bg.b7e017c0.jpg"
+  },
+  {
+    "revision": "28aaaa3ba6f8c14f64051f5a63987951",
+    "url": "/frontend-maestro/static/media/our-programs-img.28aaaa3b.jpg"
+  },
+  {
+    "revision": "1121b166c66d14c045e19403fa4d6a5d",
+    "url": "/frontend-maestro/static/media/right-arrow-notail.1121b166.svg"
+  },
+  {
+    "revision": "5b4b2290e6de14a26515d348395d9238",
+    "url": "/frontend-maestro/static/media/right-arrow.5b4b2290.svg"
+  },
+  {
+    "revision": "ce33a9b1324ae13f65d82031d0773d95",
+    "url": "/frontend-maestro/static/media/settings.ce33a9b1.svg"
+  },
+  {
+    "revision": "bcfc2f43fc546b1a5f17db5deb8f388f",
+    "url": "/frontend-maestro/static/media/shopping-bag.bcfc2f43.svg"
+  },
+  {
+    "revision": "5c1313d1c4077be3a6f006079cf8c284",
+    "url": "/frontend-maestro/static/media/sponsor-2.5c1313d1.png"
+  }
+]);
